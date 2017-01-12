60c6ced89137999317f6a0ad68093aa03560e2af
Me <535185175@qq.com>
2017-01-12 21:03:46 +0800

    Modify the AssemblyInfo.cs and rebuild dlls.

--- Source/MicroDBHelper/Properties/AssemblyInfo.cs
+++ Source/MicroDBHelper/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("MicroDBHelper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A friendly interface library to use SQLHelper.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("幻蓝工作室")]
 [assembly: AssemblyProduct("MicroDBHelper")]
-[assembly: AssemblyCopyright("Copyright © 幻蓝工作室 2017")]
+[assembly: AssemblyCopyright("https://github.com/DoraemonYu/MicroDBHelper")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.2.0")]
+[assembly: AssemblyFileVersion("3.0.2.0")]
